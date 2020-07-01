6d431913e3e29968876a93b1f425fd716ad04a43
Maiyuki <hello@maiyuki.com>
2020-07-01 13:00:08 +0200

    Menu button, javascript rotate

---
+++ dist/js/main.js
@@ -0,0 +1,34 @@
+// Select DOM items
+
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set Initial State of Menu
+let showMenu = false;
+
+burger.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    burger.classList.add("close");
+    menu.classList.add("show");
+    menuBranding.classList.add("show");
+    menuNav.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
+
+    // Set Menu State
+    showMenu = true;
+  } else {
+    burger.classList.remove("close");
+    menu.classList.remove("show");
+    menuBranding.classList.remove("show");
+    menuNav.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    // Set Menu State
+    showMenu = false;
+  }
+}
